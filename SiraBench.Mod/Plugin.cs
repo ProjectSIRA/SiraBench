dad084510d98140806993850f9ece2f80ff55937
Auros Nexus <aurosnex@gmail.com>
2021-01-02 22:32:46 -0500

    Initial commit

---
+++ SiraBench.Mod/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace SiraBench.Mod
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static IPALogger? Log { get; private set; }
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Log = logger;
+        }
+
+        [OnEnable, OnDisable]
+        public void OnState() { /* On State */ }
+    }
+}
